e9b9e29583d550b2ea6789c3b1fa5027416fe351
Vinicius Figueiredo <vinicius.figueiredo@bemobi.com>
2024-05-26 02:11:16 -0300

    Merges Collidable into Sprite.

--- src/sprites.py
+++ src/sprites.py
@@ -2,32 +2,24 @@ import pygame
 
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surface, groups):
+    def __init__(self, pos, surface, groups, hitbox_offset=None):
         super().__init__(groups)
 
         self.image = surface
         self.rect = self.image.get_rect(topleft=pos)
         self.old_rect = self.rect.copy()
 
-
-class Tile(Sprite):
-    def __init__(self, pos, surface, groups):
-        super().__init__(pos, surface, groups)
-
-
-class Collidable(Sprite):
-    def __init__(self, pos, surface, groups, hitbox_offset):
-        super().__init__(pos, surface, groups)
-
-        self.hitbox_rect = self.rect.inflate(hitbox_offset[0],
-                                             hitbox_offset[1])
-        self.old_rect = self.hitbox_rect.copy()
+        if hitbox_offset:
+            self.hitbox_rect = self.rect.inflate(hitbox_offset[0],
+                                                 hitbox_offset[1])
+            self.old_rect = self.hitbox_rect.copy()
 
 
-class AnimatedSprite(Collidable):
-    def __init__(self, pos, groups, hitbox_offset, frames, animation_speed):
+class AnimatedSprite(Sprite):
+    def __init__(self, pos, groups, frames, animation_speed, hitbox_offset=None):
         self.frames = frames
         self.frame_index = 0
+        self.hitbox_offset = hitbox_offset if hitbox_offset else None
 
         super().__init__(pos=pos,
                          surface=self.frames[self.frame_index],
@@ -42,3 +34,8 @@ class AnimatedSprite(Collidable):
 
     def update(self, dt):
         self.animate(dt)
+
+
+class Tile(Sprite):
+    def __init__(self, pos, surface, groups):
+        super().__init__(pos, surface, groups)
