a05398dba7ecb0b222a86ce37244739cf7b77fc5
Vinicius Figueiredo <vinicius.figueiredo@bemobi.com>
2024-05-27 15:43:49 -0300

    Removes unused files.

--- misc/tiled/enemy-1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="git" name="enemy-1" tilewidth="24" tileheight="24" tilecount="4" columns="4">
- <image source="../../data/graphics/enemy-1.png" width="96" height="24"/>
-</tileset>
